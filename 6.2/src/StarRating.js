1bc2bbe0aaad58e2d3c2cab09e1352dcbbeadaf9
14-shump <syun2314@gmail.com>
2023-04-23 18:41:33 +0900

    6.2

--- 6.2/src/StarRating.js
+++ 6.2/src/StarRating.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Star from "./Star";
 
-export default function StarRating({ totalStars = 5, selectedStar = 0 }) {
+export default function StarRating({ totalStars = 5, selectedStar = 0, onRate = f => f }) {
     return (
         <>
             {[...Array(totalStars)].map((n, i) => (
-                <Star key={i} selected={selectedStar > i} />
+                <Star key={i} selected={selectedStar > i} onSelect={() => onRate(i + 1)} />
             ))}
             <p>
                 {selectedStar} or {totalStars} stard
