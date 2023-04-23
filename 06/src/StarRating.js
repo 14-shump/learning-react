3f70ef31daaf481e8544d36f68d54fd71c4f7f3e
14-shump <syun2314@gmail.com>
2023-04-23 14:41:33 +0900

    6.1.2

--- 06/src/StarRating.js
+++ 06/src/StarRating.js
@@ -1,10 +1,20 @@
-import React from "react";
-import { FaStar } from "react-icons/fa";
+import React, { useState } from "react";
+import Star from "./Star";
 
-const Star = ({ selected = false }) => (
-    <FaStar color={selected ? "red" : "grey"} />
+export default function StarRating({ style = {}, totalStars = 5, ...props }) {
+  const [selectedStars, setSelectedStars] = useState(0);
+  return (
+    <div style={{ padding: "5px", ...style }} {...props}>
+        {[...Array(totalStars)].map((n, i) => (
+            <Star
+                key={i}
+                selected={selectedStars > i}
+                onSelect={() => setSelectedStars(i + 1)}
+            />
+        ))}
+        <p>
+            {selectedStars} of {totalStars} stars
+        </p>
+    </div>
   );
-
-export default function StarRating({ totalStars = 5 }) {
-  return [...Array(totalStars)].map((n, i) => <Star key={i} />);
 }
