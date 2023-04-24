93eb03799b4ea5f0cfe118c4f2612d7949aa62fa
14-shump <syun2314@gmail.com>
2023-04-24 14:59:47 +0900

    6.3.1

---
+++ 6.3/src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import colorData from "./color-data.json";
+import ColorList from "./ColorList.js";
+import AddColorForm from "./AddColorForm";
+
+export default function App() {
+  const [colors, setColors] = useState(colorData);
+  const removeColor = id => {
+    const newColors = colors.filter(color => color.id !== id);
+    setColors(newColors);
+  };
+  const rateColor = id => {
+    const newColors = colors.filter(color => color.id !== id);
+    setColors(newColors);
+  };
+
+  return (
+    <>
+      <AddColorForm
+        onNewColor={(title, color) => alert(`TODO: Create ${title} - ${color}`)}
+      />
+      <ColorList
+        colors={colors}
+        onRemoveColor={removeColor}
+        onRateColor={rateColor}
+      />
+    </>
+  );
+}
