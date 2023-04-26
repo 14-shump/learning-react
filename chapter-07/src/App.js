34b1c062850b79c4a94b941d67c66be29b2bccc3
14-shump <syun2314@gmail.com>
2023-04-26 12:16:36 +0900

    7.2 03

--- chapter-07/src/App.js
+++ chapter-07/src/App.js
@@ -1,7 +1,27 @@
-import React, { useEffect, useLayoutEffect } from "react";
+import React, { useState, useLayoutEffect } from "react";
+
+function useMousePosition() {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+
+  const setPosition = ({ x, y }) => {
+    setX(x);
+    setY(y);
+  };
+
+  useLayoutEffect(() => {
+    window.addEventListener("mousemove", setPosition);
+    return () => window.removeEventListener("mousemove", setPosition);
+  }, []);
+
+  return [x, y];
+}
 
 export default function App() {
-  useEffect(() => console.log("useEffect"));
-  useLayoutEffect(() => console.log("useLayoutEffect"));
-  return <div>ready</div>;
+  const [left, top] = useMousePosition();
+  return (
+    <div style={{ position: "absolute", top, left, cursor: "none" }}>
+      {top}x{left}
+    </div>
+  );
 }
