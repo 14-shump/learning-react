cc893a7f701fc1cea8d77ef820d711f283ac8c91
14-shump <syun2314@gmail.com>
2023-04-28 14:57:49 +0900

    8.3

--- chapter-08/src/App.js
+++ chapter-08/src/App.js
@@ -1,15 +1,31 @@
 import React, { useState, useEffect } from "react";
 
+const loadJSON = key => key && JSON.parse(localStorage.getItem(key));
+const saveJSON = (key, data) => localStorage.setItem(key, JSON.stringify(data));
+
 function GitHubUser({ login }) {
-    const [data, setData] = useState();
+    const [data, setData] = useState(loadJSON(`user:${login}`));
+
+    useEffect(() => {
+        if (!data) return;
+        if (data === login) return;
+        const { name, avatar_url, location } = data;
+        saveJSON(`user:${login}`, {
+            name,
+            login,
+            avatar_url,
+            location
+        });
+    }, [data]);
 
     useEffect(() => {
         if (!login) return;
-        fetch(`http://api.github.com/users/${login}`)
-        .then(response => response.json())
-        .then(setData)
-        .catch(console.error);
-    }, [login]);
+        if (data && data.login === login) return;
+        fetch(`https://api.github.com/users/${login}`)
+          .then(response => response.json())
+          .then(setData)
+          .catch(console.error);
+      }, [login]);
 
     if (data)
         return <pre>{JSON.stringify(data, null, 4)}</pre>
