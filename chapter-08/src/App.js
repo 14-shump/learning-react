75c9d8b70f0dbc4ebc93323d6c6150bd117df22c
14-shump <syun2314@gmail.com>
2023-04-29 12:06:30 +0900

    8.4.4

--- chapter-08/src/App.js
+++ chapter-08/src/App.js
@@ -1,35 +1,25 @@
 import React from "react";
-import { FixedSizeList } from "react-window";
-import faker from "faker";
+import Fetch from "./Fetch";
 
-const bigList = [...Array(5000)].map(() => ({
-    name: faker.name.findName(),
-    email: faker.internet.email(),
-    avatar: faker.internet.avatar()
-}));
-
-export default function App() {
-    const renderRow = ({ index, style }) => (
-        <div style={{ ...style, ...{ display: "flex" } }}>
-            <img
-                src={bigList[index].avatar}
-                alt={bigList[index].name}
-                width={50}
-            />
-            <p>
-                {bigList[index].name} - {bigList[index].email}
-            </p>
-        </div>
-    );
-
+function GitHubUser({ login }) {
     return (
-        <FixedSizeList
-            height={window.innerHeight}
-            width={window.innerWidth - 20}
-            itemCount={bigList.length}
-            itemSize={50}
-        >
-            {renderRow}
-        </FixedSizeList>
+        <Fetch
+            uri={`https://api.github.com/users/${login}`}
+            loadingFallback="loading"
+            renderError={error => {
+                return <p>Something went wrong... {error.message}</p>
+            }}
+            renderSuccess={({ data }) => (
+                <>
+                    <h1>Todo: Render UI for data</h1>
+                    <pre>{JSON.stringify(data, null, 2)}</pre>
+                </>
+            )}
+
+        />
     );
 }
+
+export default function App() {
+    return <GitHubUser login="14-shump" />;
+  }
