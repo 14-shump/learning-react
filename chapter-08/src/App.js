9bab52dfead5c89ed75eee5d57f1753156fc9c8c
14-shump <syun2314@gmail.com>
2023-04-29 09:59:02 +0900

    8.4

--- chapter-08/src/App.js
+++ chapter-08/src/App.js
@@ -1,36 +1,36 @@
 import React, { useState, useEffect } from "react";
 
-const loadJSON = key => key && JSON.parse(localStorage.getItem(key));
-const saveJSON = (key, data) => localStorage.setItem(key, JSON.stringify(data));
-
 function GitHubUser({ login }) {
-    const [data, setData] = useState(loadJSON(`user:${login}`));
-
-    useEffect(() => {
-        if (!data) return;
-        if (data === login) return;
-        const { name, avatar_url, location } = data;
-        saveJSON(`user:${login}`, {
-            name,
-            login,
-            avatar_url,
-            location
-        });
-    }, [data]);
+    const [data, setData] = useState();
+    const [error, setError] = useState();
+    const [loding, setLoding] = useState(false);
 
     useEffect(() => {
         if (!login) return;
-        if (data && data.login === login) return;
+        setLoding(true);
         fetch(`https://api.github.com/users/${login}`)
-          .then(response => response.json())
+          .then(data => data.json())
           .then(setData)
-          .catch(console.error);
+          .then(() => setLoding(false))
+          .catch(setError);
       }, [login]);
 
-    if (data)
-        return <pre>{JSON.stringify(data, null, 4)}</pre>
+    if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
+    if (loding) return <h1>loding...</h1>
+    if (!data) return null
 
-    return null;
+    console.log(data);
+
+    return (
+        <div className="githubuser">
+            <img src={data.avatar_url} alt={data.login} style={{ width: 200 }} />
+            <div>
+                <h1>{data.login}</h1>
+                {data.name && <p>{data.name}</p>}
+                {data.location && <p>{data.location}</p>}
+            </div>
+        </div>
+    );
 }
 
 export default function App() {
