a960eaced37e33978293bb353a29b9d9aeb01d29
14-shump <syun2314@gmail.com>
2023-05-14 12:31:31 +0900

    10.6.2

--- chapter-10/src/Star.test.js
+++ chapter-10/src/Star.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Star from "./Star";
 import { toHaveAttribute } from "@testing-library/jest-dom";
+import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
 expect.extend({ toHaveAttribute });
@@ -10,4 +11,10 @@ test("renders a star", () => {
   const div = document.createElement("div");
   ReactDOM.render(<Star />, div);
   expect(div.querySelector("svg")).toHaveAttribute("id", "star");
+});
+
+test("renders an h1", () => {
+  const { getByText } = render(<Star />);
+  const h1 = getByText(/Great Star/);
+  expect(h1).toHaveTextContent("Great Star");
 });
