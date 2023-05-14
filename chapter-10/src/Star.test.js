c7c4269f0a5bf8b907be95445da95a5eeef792b7
14-shump <syun2314@gmail.com>
2023-05-14 12:30:31 +0900

    10.6.1

--- chapter-10/src/Star.test.js
+++ chapter-10/src/Star.test.js
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Star from "./Star";
+import { toHaveAttribute } from "@testing-library/jest-dom";
+import "@testing-library/jest-dom/extend-expect";
+
+expect.extend({ toHaveAttribute });
 
 test("renders a star", () => {
   const div = document.createElement("div");
   ReactDOM.render(<Star />, div);
-  expect(div.querySelector("svg")).toBeTruthy();
+  expect(div.querySelector("svg")).toHaveAttribute("id", "star");
 });
