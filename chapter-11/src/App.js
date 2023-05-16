74c09ed3a8cca37a0c7e8cda65c3f944dbca432d
14-shump <syun2314@gmail.com>
2023-05-16 11:58:07 +0900

    11.2、11.3、11.4

--- chapter-11/src/App.js
+++ chapter-11/src/App.js
@@ -5,16 +5,33 @@ import {
   About,
   Events,
   Products,
-  Contact
+  Contact,
+  Whoops404,
+  Services,
+  History,
+  Location
 } from "./pages";
 
 function App() {
   let element = useRoutes([
     { path: "/", element: <Home /> },
-    { path: "/about", element: <About /> },
+    {
+      path: "/about",
+      element: <About />,
+      children: [
+        { path: "services", element: <Services /> },
+        { path: "history", element: <History /> },
+        { path: "location", element: <Location /> },
+      ]
+    },
     { path: "/events", element: <Events /> },
     { path: "/products", element: <Products /> },
-    { path: "/contact", element: <Contact /> }
+    { path: "/contact", element: <Contact /> },
+    { path: "*", element: <Whoops404 /> },
+    {
+      path: "services",
+      redirectTo: "about/services"
+    }
   ]);
 
   return element;
--- chapter-11/src/pages.js
+++ chapter-11/src/pages.js
@@ -1,5 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {
+  Link,
+  useLocation,
+  Outlet
+} from "react-router-dom";
 
 export function Home() {
   return (
@@ -19,7 +23,79 @@ export function About() {
   return (
     <div>
       <h1>[About]</h1>
+      <Outlet />
+      <nav>
+        <Link to="services">Services</Link>
+        <Link to="history">History</Link>
+        <Link to="location">Location</Link>
+      </nav>
     </div>
+  );
+}
+
+export function Services() {
+  return (
+    <section>
+      <h2>Our Services</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Integer nec odio.
+        Praesent libero. Sed cursus ante dapibus
+        diam. Sed nisi. Nulla quis sem at nibh
+        elementum imperdiet. Duis sagittis ipsum.
+        Praesent mauris. Fusce nec tellus sed
+        augue semper porta. Mauris massa.
+        Vestibulum lacinia arcu eget nulla. Class
+        aptent taciti sociosqu ad litora torquent
+        per conubia nostra, per inceptos
+        himenaeos. Curabitur sodales ligula in
+        libero.
+      </p>
+    </section>
+  );
+}
+
+export function History() {
+  return (
+    <section>
+      <h2>Our History</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Integer nec odio.
+        Praesent libero. Sed cursus ante dapibus
+        diam. Sed nisi. Nulla quis sem at nibh
+        elementum imperdiet. Duis sagittis ipsum.
+        Praesent mauris. Fusce nec tellus sed
+        augue semper porta. Mauris massa.
+        Vestibulum lacinia arcu eget nulla. Class
+        aptent taciti sociosqu ad litora torquent
+        per conubia nostra, per inceptos
+        himenaeos. Curabitur sodales ligula in
+        libero.
+      </p>
+    </section>
+  );
+}
+
+export function Location() {
+  return (
+    <section>
+      <h2>Our Location</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Integer nec odio.
+        Praesent libero. Sed cursus ante dapibus
+        diam. Sed nisi. Nulla quis sem at nibh
+        elementum imperdiet. Duis sagittis ipsum.
+        Praesent mauris. Fusce nec tellus sed
+        augue semper porta. Mauris massa.
+        Vestibulum lacinia arcu eget nulla. Class
+        aptent taciti sociosqu ad litora torquent
+        per conubia nostra, per inceptos
+        himenaeos. Curabitur sodales ligula in
+        libero.
+      </p>
+    </section>
   );
 }
 
@@ -43,6 +119,16 @@ export function Contact() {
   return (
     <div>
       <h1>[Contact]</h1>
+    </div>
+  );
+}
+
+export function Whoops404() {
+  let location = useLocation();
+  console.log(location);
+  return (
+    <div>
+      <h1>Resource not found at {location.pathname}</h1>
     </div>
   );
 }
